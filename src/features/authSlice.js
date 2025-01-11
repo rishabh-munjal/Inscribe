d4d164e74495117f0c66b8627d333b5341a3b789
Rishabh Munjal <rishabh5566ind1111@gmail.com>
2025-01-11 18:15:15 +0530

    appwrite+redux

---
+++ src/features/authSlice.js
@@ -0,0 +1,28 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  status: false, // Default state value
+  userData: null,
+};
+
+export const authSlice = createSlice({
+  name: "auth", // Name of the slice
+  initialState,
+  reducers: {
+    login: (state, action) => {
+      state.status = true;
+      state.userData = action.payload.userData;
+    },
+
+    logout: (state) => {
+      state.status = false;
+      state.userData = null;
+    },
+  },
+});
+
+// Export actions for use in components
+export const { login, logout } = authSlice.actions;
+
+// Export reducer for store configuration
+export default authSlice.reducer;
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './src/**/*.{html,js,jsx,ts,tsx}', // Adjust based on your project structure
+    './public/index.html',            // Include the main HTML file if applicable
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
